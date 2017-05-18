235979b297001af6afe5d6981d051cbdac9022bc
ringlayer <jasapluscom@yahoo.com>
2017-05-19 02:08:31 +0700

    Add files via upload

--- gen32.sh
+++ gen32.sh
@@ -25,22 +25,15 @@ chroot $HOME/live_boot/chroot  /bin/bash -c "uname -a; \
 sleep 3; \
 echo debian-live-x86 > /etc/hostname; \
 apt-get update; \
-apt-get install --no-install-recommends --yes --force-yes live-boot  network-manager net-tools wireless-tools wpagui; \
-apt-get install --no-install-recommends --yes --force-yes tcpdump wget openssh-client blackbox xserver-xorg-core xserver-xorg xinit xterm pciutils usbutils gparted; \
-apt-get intall --no-install-recommends --yes --force-yes syslinux partclone nano pv rtorrent iceweasel chntpw ntfs-3g hfsprogs rsync dosfstools; \
-apt-get install --no-install-recommends --yes --force-yes linux-image-3.16.0-4-586; \
+apt-get install  --yes --force-yes live-boot  network-manager net-tools wireless-tools wpagui tcpdump wget openssh-client blackbox xserver-xorg-core xserver-xorg xinit xterm pciutils usbutils gparted syslinux partclone nano pv rtorrent iceweasel chntpw ntfs-3g hfsprogs rsync dosfstools; \
+apt-get install  --yes --force-yes linux-image-3.16.0-4-586; \
 apt-get clean"
 
 mkdir -p $HOME/live_boot/image/{live,isolinux}
 
-(cd $HOME/live_boot && \
-    sudo mksquashfs chroot image/live/filesystem.squashfs -e boot
-)
+(cd $HOME/live_boot && mksquashfs chroot image/live/filesystem.squashfs -e boot)
 
-(cd $HOME/live_boot && \
-    cp chroot/boot/vmlinuz-3.16.0-4-586 image/live/vmlinuz1
-    cp chroot/boot/initrd.img-3.16.0-4-586 image/live/initrd1
-)
+(cd $HOME/live_boot &&  cp chroot/boot/vmlinuz-3.16.0-4-586 image/live/vmlinuz1 && cp chroot/boot/initrd.img-3.16.0-4-586 image/live/initrd1)
 
 
 cat > $HOME/live_boot/image/isolinux/isolinux.cfg <<- EOM
@@ -67,17 +60,4 @@ cd $HOME/live_boot/image/ && \
     cp /usr/lib/syslinux/modules/bios/libgpl.c32 isolinux/ && \
     cp /usr/share/misc/pci.ids isolinux
 
-genisoimage \
-    -rational-rock \
-    -volid "Debian Live" \
-    -cache-inodes \
-    -joliet \
-    -hfs \
-    -full-iso9660-filenames \
-    -b isolinux/isolinux.bin \
-    -c isolinux/boot.cat \
-    -no-emul-boot \
-    -boot-load-size 4 \
-    -boot-info-table \
-    -output $HOME/live_boot/debian-live-x86.iso \
-    $HOME/live_boot/image
+genisoimage  -rational-rock  -volid "Debian Live"  -cache-inodes -joliet  -hfs  -full-iso9660-filenames  -b isolinux/isolinux.bin  -c isolinux/boot.cat  -no-emul-boot  -boot-load-size 4  -boot-info-table   -output $HOME/live_boot/debian-live-x86.iso  $HOME/live_boot/image
